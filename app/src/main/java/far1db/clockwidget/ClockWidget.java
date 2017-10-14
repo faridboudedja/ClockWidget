40abb05a794df765bcfd4f3be088e98642e1123a
far1db <32672049+far1db@users.noreply.github.com>
2017-10-14 11:02:36 +0100

    Display today's date on the widget

--- app/src/main/java/far1db/clockwidget/ClockWidget.java
+++ app/src/main/java/far1db/clockwidget/ClockWidget.java
@@ -5,11 +5,17 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class ClockWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-
+        // Update all instances of the widget
+        for (int appWidgetId : appWidgetIds) {
+            updateWidgetDate(context, appWidgetManager, appWidgetId);
+        }
     }
 
     @Override
@@ -20,6 +26,23 @@ public class ClockWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    private void updateWidgetDate(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        // Get date
+        Date date = new Date();
+        date.getTime();
+
+        // Format the date according to current Locale
+        String dateStr = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+
+        // Get the remote views of the widget's layout
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock_widget);
+        // Display the date in the appropriate text view
+        views.setTextViewText(R.id.tv_date, dateStr);
+
+        // Update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
 
